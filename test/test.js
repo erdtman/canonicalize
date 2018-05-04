63a4176665b1d4fb98cea898242eaaf68d8414fe
Samuel Erdtman <erdtman@spotify.com>
2018-05-04 12:58:33 +0200

    added testcase for object with one atribute

--- test/test.js
+++ test/test.js
@@ -9,35 +9,35 @@ const fs = require('fs');
 
 test('arrays', t => {
   const input = jsonfile.readFileSync('test/testdata/input/arrays.json');
-  const expected = fs.readFileSync('test/testdata/output/arrays.json', 'utf8');
+  const expected = fs.readFileSync('test/testdata/output/arrays.json', 'utf8').trim();
   const actual = JSON.canonicalize(input);
   t.is(actual, expected);
 });
 
 test('french', t => {
   const input = jsonfile.readFileSync('test/testdata/input/french.json');
-  const expected = fs.readFileSync('test/testdata/output/french.json', 'utf8');
+  const expected = fs.readFileSync('test/testdata/output/french.json', 'utf8').trim();
   const actual = JSON.canonicalize(input);
   t.is(actual, expected);
 });
 
 test('structures', t => {
   const input = jsonfile.readFileSync('test/testdata/input/structures.json');
-  const expected = fs.readFileSync('test/testdata/output/structures.json', 'utf8');
+  const expected = fs.readFileSync('test/testdata/output/structures.json', 'utf8').trim();
   const actual = JSON.canonicalize(input);
   t.is(actual, expected);
 });
 
 test('values', t => {
   const input = jsonfile.readFileSync('test/testdata/input/values.json');
-  const expected = fs.readFileSync('test/testdata/output/values.json', 'utf8');
+  const expected = fs.readFileSync('test/testdata/output/values.json', 'utf8').trim();
   const actual = JSON.canonicalize(input);
   t.is(actual, expected);
 });
 
 test('values', t => {
   const input = jsonfile.readFileSync('test/testdata/input/weird.json');
-  const expected = fs.readFileSync('test/testdata/output/weird.json', 'utf8');
+  const expected = fs.readFileSync('test/testdata/output/weird.json', 'utf8').trim();
   const actual = JSON.canonicalize(input);
   t.is(actual, expected);
 });
